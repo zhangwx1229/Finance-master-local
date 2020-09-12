dc5d20980767c0122d3111b978046162686b83ee
张惟新 <zhangwx01@mail.jj.cn>
2020-09-12 14:37:13 +0800

    提交修改bar的字体

--- src/navigator/TabNav/index.js
+++ src/navigator/TabNav/index.js
@@ -7,8 +7,8 @@
  * @FilePath: /lvsejunying/src/navigator/TabNav/index.js
  */
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Image, StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../../screens/Main/TabHomeNew/HomeScreen';
 import MineScreen from '../../screens/Main/TabMine/MineScreen';
 import UI from '../../UI';
@@ -18,74 +18,76 @@ import Images from '../../image';
 const Tab = createBottomTabNavigator();
 
 export default function TabNav() {
-  return (
-    <Tab.Navigator
-      tabBarOptions={{
-        activeTintColor: UI.color.primary,
-        inactiveTintColor: UI.color.darkGray,
-      }}
-    >
-      <Tab.Screen
-        name="TabHome"
-        component={HomeScreen}
-        options={{
-          tabBarLabel: '首页',
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              source={focused ? Images.tab_home_selected : Images.tab_home}
-              style={styles.icon}
+    const font = UI.fontSizeNew.font_10
+    return (
+        < Tab.Navigator
+            tabBarOptions={{
+                activeTintColor: UI.color.primary,
+                inactiveTintColor: UI.color.darkGray,
+                labelStyle: { fontSize: font }
+            }}
+        >
+            <Tab.Screen
+                name="TabHome"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: '首页',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Image
+                            source={focused ? Images.tab_home_selected : Images.tab_home}
+                            style={styles.icon}
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Learn"
-        component={TaxScreen}
-        options={{
-          tabBarLabel: '办税',
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              source={focused ? Images.tab_tax_selected : Images.tab_tax}
-              style={styles.icon}
+            <Tab.Screen
+                name="Learn"
+                component={TaxScreen}
+                options={{
+                    tabBarLabel: '办税',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Image
+                            source={focused ? Images.tab_tax_selected : Images.tab_tax}
+                            style={styles.icon}
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Note"
-        component={ServiceScreen}
-        options={{
-          tabBarLabel: '服务',
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              source={
-                focused ? Images.tab_service_selected : Images.tab_service
-              }
-              style={styles.icon}
+            <Tab.Screen
+                name="Note"
+                component={ServiceScreen}
+                options={{
+                    tabBarLabel: '服务',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Image
+                            source={
+                                focused ? Images.tab_service_selected : Images.tab_service
+                            }
+                            style={styles.icon}
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Mine"
-        component={MineScreen}
-        options={{
-          tabBarLabel: '个人中心',
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              source={focused ? Images.tab_mine_selected : Images.tab_mine}
-              style={styles.icon}
+            <Tab.Screen
+                name="Mine"
+                component={MineScreen}
+                options={{
+                    tabBarLabel: '个人中心',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Image
+                            source={focused ? Images.tab_mine_selected : Images.tab_mine}
+                            style={styles.icon}
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+        </Tab.Navigator >
+    );
 }
 
 const styles = StyleSheet.create({
-  icon: {
-    width: 28,
-    height: 28,
-  },
+    icon: {
+        width: 28,
+        height: 28,
+    },
 });
