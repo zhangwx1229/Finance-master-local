95ad5b44621b6a121d029547c7525a8452291860
张惟新 <zhangwx01@mail.jj.cn>
2021-01-01 19:25:28 +0800

    提交身份证信息

--- src/screens/Main/TabMine/MineScreen/MineTaxPreference.js
+++ src/screens/Main/TabMine/MineScreen/MineTaxPreference.js
@@ -30,14 +30,18 @@ export default class MineTaxPreference extends PureComponent {
         if (this.data === null) {
             return null
         }
+        const image_top = UI.size.screenWidth * 130 / 1080
+        const image_bottom = UI.size.screenWidth * 202 / 1080
         const scre_w = UI.size.screenWidth - 40 * 2
+        const image_h = scre_w * 494 / 818
+        const top = (UI.size.screenHeight - image_top - image_bottom - image_h) / 2
         return (<View style={styles.container} >
-            <Image style={{ position: 'absolute', width: UI.size.screenWidth, height: UI.size.screenWidth * 130 / 1080 }} source={Images.icon_6} />
+            <Image style={{ position: 'absolute', width: UI.size.screenWidth, height: image_top }} source={Images.icon_6} />
             <TouchableWithoutFeedback onPress={this.back}>
                 <View style={{ marginLeft: 5, width: 50, height: 40 }} />
             </TouchableWithoutFeedback>
-            <Image style={{ position: 'absolute', left: 40, top: (UI.size.screenHeight - scre_w * 494 / 818 - 100) / 2, width: scre_w, height: scre_w * 494 / 818 }} source={Images.icon_7} />
-            <Image style={{ position: 'absolute', bottom: 0, width: UI.size.screenWidth, height: UI.size.screenWidth * 202 / 1080 }} source={Images.icon_8} />
+            <Image style={{ position: 'absolute', left: 40, top: top, width: scre_w, height: image_h }} source={Images.icon_7} />
+            <Image style={{ position: 'absolute', bottom: 0, width: UI.size.screenWidth, height: image_bottom }} source={Images.icon_8} />
         </View >
         );
     }
