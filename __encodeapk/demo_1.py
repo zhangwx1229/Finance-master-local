f156cd812ea8a5f840411c17316b7886793818d9
zhangwx <zhangwx01@mail.jj.cn>
2021-01-26 20:21:37 +0800

    提交文本

--- __encodeapk/demo_1.py
+++ __encodeapk/demo_1.py
@@ -10,7 +10,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-filename = "text_02.xlsx"
+filename = "个税样板.xlsx"
 json_file = 'filename.json'
 filePath = os.path.join(os.getcwd(), filename)
 pathDir = '../../个税'
