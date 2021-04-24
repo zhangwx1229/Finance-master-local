7c59e82764047ba82c795a20475b98d94d567c4b
张惟新 <zhangwx01@mail.jj.cn>
2021-04-24 08:28:00 +0800

    提交

--- __encodeapk/demo_1.py
+++ __encodeapk/demo_1.py
@@ -9,7 +9,7 @@ import subprocess
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-filename = "关登芝北京通样板 -.xlsx"
+filename = "闻秀艳北京通样板 -.xlsx"
 json_file = 'filename.json'
 filePath = os.path.join(os.getcwd(), filename)
 pathDir = '../../北京通'
